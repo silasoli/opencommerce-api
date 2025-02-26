142b1ee8c344125355a3a217bc61d20f256c4e6b
silasoli <silasoliv39@gmail.com>
2025-02-25 21:18:50 -0300

    chore: test pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+  ApiAcceptedResponse,
+} from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 import { SERVER_ERRORS } from './common/constants/server.errors';
@@ -39,6 +43,7 @@ async function bootstrap() {
       tagsSorter: 'alpha',
       operationsSorter: 'alpha',
       filter: true,
+      docExpansion: false,
     },
     customSiteTitle: 'Open Commerce API Docs',
     customfavIcon:
