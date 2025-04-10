3101b5ff25b37a6b0eb93899c5fb28825404a70d
silasoli <silasoliv39@gmail.com>
2025-04-09 21:37:31 -0300

    feat: adding db and users initial resource

---
+++ src/common/exception-filters/entity-not-found-exception.filter.ts
@@ -0,0 +1,67 @@
+import {
+  Catch,
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+import { EntityNotFoundError } from 'typeorm';
+
+@Catch(EntityNotFoundError)
+export class EntityNotFoundExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(EntityNotFoundExceptionFilter.name);
+
+  catch(exception: EntityNotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const field = this.extractFieldFromErrorMessage(exception.message);
+
+    const message =
+      field && TypeORMExceptionMessages[field]
+        ? [TypeORMExceptionMessages[field]]
+        : ['Recurso não encontrado.'];
+
+    this.logger.warn(`Entity not found: ${exception.message}`);
+
+    return this.sendErrorResponse(
+      response,
+      HttpStatus.NOT_FOUND,
+      request.url,
+      'Entidade não encontrada.',
+      message,
+    );
+  }
+
+  private sendErrorResponse(
+    response: Response,
+    status: HttpStatus,
+    path: string,
+    error: string,
+    message: string[],
+  ): void {
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      path: path,
+      error: {
+        message: message,
+        error: error,
+        statusCode: status,
+      },
+    });
+  }
+
+  private extractFieldFromErrorMessage(message: string): string | null {
+    if (!message) return null;
+    const match = message.match(/entity of type "([^"]+)"/i);
+    return match
+      ? match[1].replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
+      : null;
+  }
+}
+
+export const TypeORMExceptionMessages: Record<string, string> = {
+  product: 'Produto não encontrado.',
+};
